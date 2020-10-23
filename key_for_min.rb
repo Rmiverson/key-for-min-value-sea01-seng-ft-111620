5c344ac26ea917827436623a5fb6000caa111341
Riley Iverson <rmiverson99@gmail.com>
2020-10-23 23:53:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if !smallest_value
       smallest_value = value
+      ans = key
     elsif value <= smallest_value
       smallest_value = value
       ans = key
